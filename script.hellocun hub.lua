a268abf488a713c1613441bc1218d9a8d8d8adba
helocoolm <hoangnu195050@gmail.com>
2025-08-31 20:53:12 +0700

    Add files via upload

--- script.hellocun hub.lua
+++ script.hellocun hub.lua
@@ -3,6 +3,7 @@ loadstring(game:HttpGet(("https://raw.githubusercontent.com/daucobonhi/Ui-Redz-V
        local Window = MakeWindow({
          Hub = {
          Title = "hellocun hub",
+         SubTitle = "v2.3.3",
          Animation = "người tạo script: Hellocun"
          },
         Key = {
@@ -22,7 +23,7 @@ loadstring(game:HttpGet(("https://raw.githubusercontent.com/daucobonhi/Ui-Redz-V
 
        MinimizeButton({
        Image = "http://www.roblox.com/asset/?id=96543430071872",
-       Size = {70, 60},
+       Size = {60, 60},
        Color = Color3.fromRGB(10, 10, 10),
        Corner = true,
        Stroke = false,
@@ -30,9 +31,26 @@ loadstring(game:HttpGet(("https://raw.githubusercontent.com/daucobonhi/Ui-Redz-V
       })
       
 ------ Tab
-     local Tab1o = MakeTab({Name = "Script Farm"})
-     
+     local Tab0o = MakeTab({Name = "thông tin"})
+     local Tab1o = MakeTab({Name = "Script Farm blox fr"})
+     local Tab2o = MakeTab({Name = "Script thối não"})
+     local Tab3o = MakeTab({Name = "script 99 ngày"})
 ------- BUTTON
+    
+    AddButton(Tab0o, {
+    Title = "Youtuber",
+    Description = "vtd roblox ",
+    Callback = function()
+        setclipboard("https://youtube.com/@vtd.roblox1?si=TOynN8y3a6s7K9aC")
+    end
+});
+    AddButton(Tab0o, {
+    Title = "Discord",
+    Description = "Giao Lưu",
+    Callback = function()
+        setclipboard("https://https://discord.gg/VDzUra2WFK");
+    end
+});
     
     AddButton(Tab1o, {
      Name = "Redz Hub",
@@ -44,3 +62,37 @@ loadstring(game:HttpGet(("https://raw.githubusercontent.com/daucobonhi/Ui-Redz-V
 loadstring(game:HttpGet("https://raw.githubusercontent.com/realredz/BloxFruits/refs/heads/main/Source.lua"))(Settings)
   end
   })
+  
+  AddButton(Tab2o, {
+     Name = "script 99day no key",
+    Callback = function()
+	  local Settings = {
+loadstring(game:HttpGet("https://raw.githubusercontent.com/VapeVoidware/VW-Add/main/nightsintheforest.lua", true))()
+end
+ })
+ 
+ AddButton(Tab2o, {
+     Name = "script 99day ",
+    Callback = function()
+	  local Settings = {
+loadstring(game:HttpGet("https://raw.githubusercontent.com/H4xScripts/Loader/refs/heads/main/loader.lua", true))()
+end
+ })
+ 
+ AddButton(Tab3o, {
+     Name = "Script thối não no key",
+    Callback = function()
+	  local Settings = {
+loadstring(game:HttpGet("https://raw.githubusercontent.com/tienkhanh1/spicy/main/Chilli.lua"))()
+end
+  })
+
+AddButton(Tab1o, {
+     Name = "Script speed hub",
+    Callback = function()
+	  local Settings = {
+loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))() 
+end
+ })
+
+
